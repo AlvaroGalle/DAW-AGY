74605dd6ad9e57a305febfd4b1e8a0d075da7ab0
AlvaroGalle <alvarogy2004@gmal.com>
2023-09-15 12:49:23 +0200

    first commit

---
+++ subir.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+echo "Actualizando repositorio DESPLIEGUE de Aplicaciones Web"
+echo "-----------------------TOKEN---------------------------------"
+echo "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"
+echo "-----------------------TOKEN---------------------------------"
+echo "PASO 1: Actualizar carpeta local"
+git pull
+
+echo "PASO 2: Añadimos archivos locales"
+git add .
+
+echo "PASO 3: Descripcion de subia"
+read -p "Escribe el mensaje de subida" mensaje
+git commit -m "$mensaje" .
+echo "PASO 4: Realizar Subida"
+git push
+
